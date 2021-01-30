dce73a0302201e621e3e032d3956ea63a4ce5e0e
Pantanetti <DPantanetti@traditum.com>
2021-01-30 20:39:25 -0300

    TallerMusso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
+declare function init($);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TallerMusso';
+
+
+export class AppComponent implements OnInit {
+  title = 'Taller Musso';
+
+  ngOnInit(): void {
+
+    // SLIDER
+    init($);
+
+  }
 }
